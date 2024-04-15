c6e96364780f504b00f9cfeaec266afa18347e7c
jin song <songjin3914@gmail.com>
2024-04-15 11:56:01 +0900

    메인완성

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,12 @@
 import 'package:flutter/material.dart';
 
 import 'package:provider/provider.dart';
+import 'package:subway_search/data/data_source/subway_data_source.dart';
+import 'package:subway_search/presentation/search_list_view_model.dart';
+import 'package:subway_search/presentation/search_main.dart';
+
+import 'data/repository/subway_repository.dart';
+import 'data/repository/subway_repository_impl.dart';
 
 void main() {
   runApp(const MyApp());
@@ -18,12 +24,12 @@ class MyApp extends StatelessWidget {
         useMaterial3: true,
       ),
       home: ChangeNotifierProvider(   // cnp
-        create: (_) => SearchMain(
-          photoRepository: SubwayRepositoryImpl(
-            photoDataSource: PhotoDataSource(),
-          ),
+        create: (_) => SearchListViewModel(
+            subwayRepository: SubwayRepositoryImpl(
+                subWayDataSource: SubWayDataSource()
+            )
         ),
-        child: const SearchListScreen(),
+        child: const SearchMain(),
       ),
     );
   }
